28c31b9eb7d99eaeb1cbb6b9d089cc835ca28f55
Huexxx <huexxx@gmail.com>
2023-05-10 22:56:45 +0200

    Spoof isGKI()

--- manager/app/src/main/java/me/weishu/kernelsu/Kernels.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/Kernels.kt
@@ -24,7 +24,7 @@ data class KernelVersion(val major: Int, val patchLevel: Int, val subLevel: Int)
             return patchLevel >= 10
         }
 
-        return false
+        return true
     }
 }
 
